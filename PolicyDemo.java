bc874d3ae21d273141c891fbdbd1d27f6ec37694
gvelazc0 <69766792+gvelazc0@users.noreply.github.com>
2021-03-03 21:32:58 -0500

    Update PolicyDemo.java

--- PolicyDemo.java
+++ PolicyDemo.java
@@ -5,47 +5,47 @@ public class PolicyDemo
    public static void main(String[] args)
    {
       //Declare variables for user input
-      String demoPolicyNumber, //to hold the Policy Number
-             demoProviderName, //to hold the Provider Name
-             demoFirstName, //to hold the Policyholder's First Name
-             demoLastName, //to hold the Policyholder's Last Name
-             demoSmokingStatus; //to hold the Policyholder's Smoking Status                  
-      int demoAge; //to hold the Policyholder's Age
-      double demoHeight, //to hold the Policyholder's Height in inches
-             demoWeight; //to hold the Policyholder's Weight in pounds
+      String policyNumber, //to hold the Policy Number
+             providerName, //to hold the Provider Name
+             firstName, //to hold the Policyholder's First Name
+             lastName, //to hold the Policyholder's Last Name
+             smokingStatus; //to hold the Policyholder's Smoking Status                  
+      int age; //to hold the Policyholder's Age
+      double height, //to hold the Policyholder's Height in inches
+             weight; //to hold the Policyholder's Weight in pounds
    
       //Create Scanner object for input
       Scanner keyboard = new Scanner(System.in);
      
       //Ask user for info
       System.out.print("Please enter the Policy Number: "); 
-         demoPolicyNumber = keyboard.nextLine();  
+         policyNumber = keyboard.nextLine();  
 
       System.out.print("Please enter the Provider Name: "); 
-         demoProviderName = keyboard.nextLine();  
+         providerName = keyboard.nextLine();  
 
       System.out.print("Please enter the Policyholder's First Name: "); 
-         demoFirstName = keyboard.nextLine();  
+         firstName = keyboard.nextLine();  
 
       System.out.print("Please enter the Policyholder's Last Name: "); 
-         demoLastName = keyboard.nextLine();  
+         lastName = keyboard.nextLine();  
 
       System.out.print("Please enter the Policyholder's Age: "); 
-         demoAge = keyboard.nextInt();  
+         age = keyboard.nextInt();  
          
       keyboard.nextLine(); //remmove trailing
    
       System.out.print("Please enter the Policyholder's Smoking Status (smoker/non-smoker): "); 
-         demoSmokingStatus = keyboard.nextLine();  
+         smokingStatus = keyboard.nextLine();  
        
       System.out.print("Please enter the Policyholder's Height (in inches): "); 
-         demoHeight = keyboard.nextDouble();  
+         height = keyboard.nextDouble();  
    
       System.out.print("Please enter the Policyholder's Weight (in pounds): "); 
-         demoWeight = keyboard.nextDouble();  
+         weight = keyboard.nextDouble();  
                 
       //Create an instance of the Policy class
-      Policy instance1 = new Policy(demoPolicyNumber, demoProviderName, demoFirstName, demoLastName, demoAge, demoSmokingStatus, demoHeight, demoWeight);
+      Policy instance1 = new Policy(policyNumber, providerName, firstName, lastName, age, smokingStatus, height, weight);
       
       //Display data
       System.out.println("Policy Number: " + instance1.getPolicyNumber());
@@ -57,7 +57,7 @@ public class PolicyDemo
       System.out.printf("Policyholder's Height: %.1f inches", instance1.getHeight());
       System.out.printf("\nPolicyholder's Weight: %.1f pounds", instance1.getWeight());
       System.out.printf("\nPolicyholder's BMI: %.2f", instance1.getBMI());
-      System.out.printf("\nPolicy Price: $%.2f", instance1.getPricePolicy(demoAge,demoSmokingStatus));
+      System.out.printf("\nPolicy Price: $%.2f", instance1.getPricePolicy(age, smokingStatus));
    
    } //end main
 } //end class
