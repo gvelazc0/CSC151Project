cbfcb23229d44701e7ffa6c494afad1a5f24764d
gvelazc0 <69766792+gvelazc0@users.noreply.github.com>
2021-03-16 20:35:16 -0400

    Update PolicyDemo.java

--- PolicyDemo.java
+++ PolicyDemo.java
@@ -1,4 +1,5 @@
-import java.util.Scanner;
+import java.util.Scanner; //Needed for Scanner class
+import java.io.*; //Needed for File class
 
 public class PolicyDemo
 {
@@ -14,50 +15,42 @@ public class PolicyDemo
       double height, //to hold the Policyholder's Height in inches
              weight; //to hold the Policyholder's Weight in pounds
    
-      //Create Scanner object for input
-      Scanner keyboard = new Scanner(System.in);
-     
-      //Ask user for info
-      System.out.print("Please enter the Policy Number: "); 
-         policyNumber = keyboard.nextLine();  
-
-      System.out.print("Please enter the Provider Name: "); 
-         providerName = keyboard.nextLine();  
-
-      System.out.print("Please enter the Policyholder's First Name: "); 
-         firstName = keyboard.nextLine();  
-
-      System.out.print("Please enter the Policyholder's Last Name: "); 
-         lastName = keyboard.nextLine();  
-
-      System.out.print("Please enter the Policyholder's Age: "); 
-         age = keyboard.nextInt();  
-         
-      keyboard.nextLine(); //remmove trailing
-   
-      System.out.print("Please enter the Policyholder's Smoking Status (smoker/non-smoker): "); 
-         smokingStatus = keyboard.nextLine();  
-       
-      System.out.print("Please enter the Policyholder's Height (in inches): "); 
-         height = keyboard.nextDouble();  
-   
-      System.out.print("Please enter the Policyholder's Weight (in pounds): "); 
-         weight = keyboard.nextDouble();  
-                
-      //Create an instance of the Policy class
-      Policy instance1 = new Policy(policyNumber, providerName, firstName, lastName, age, smokingStatus, height, weight);
       
-      //Display data
-      System.out.println("Policy Number: " + instance1.getPolicyNumber());
-      System.out.println("Provider Name: " + instance1.getProviderName());
-      System.out.println("Policyholder's First Name: " + instance1.getFirstName());
-      System.out.println("Policyholder's Last Name: " + instance1.getLastName());
-      System.out.println("Policyholder's Age: " + instance1.getAge());
-      System.out.println("Policyholder's Smoking Status: " + instance1.getSmokingStatus());
-      System.out.printf("Policyholder's Height: %.1f inches", instance1.getHeight());
-      System.out.printf("\nPolicyholder's Weight: %.1f pounds", instance1.getWeight());
-      System.out.printf("\nPolicyholder's BMI: %.2f", instance1.getBMI());
-      System.out.printf("\nPolicy Price: $%.2f", instance1.getPricePolicy(age, smokingStatus));
+      //Read the file
+      File myfile = new File("PolicyInformation.txt"); //open the file
+      Scanner inputFile = new Scanner(myfile); //create a Scanner object to read the file
+      
+      while (inputFile.hasNext()) //loop processes the lines until the end of the file
+      {
+         policyNumber = inputFile.nextLine();
+         providerName = inputFile.nextLine();
+         firstName = inputFile.nextLine();
+         lastName = inputFile.nextLine();
+         age = inputFile.nextInt();
+         smokingStatus = inputFile.nextLine();
+         height = inputFile.nextDouble();
+         weight = inputFile.nextDouble();
+                  
+         
+    
+           
+         //Display data
+         System.out.println("Policy Number: " + instance1.getPolicyNumber());
+         System.out.println("Provider Name: " + instance1.getProviderName());
+         System.out.println("Policyholder's First Name: " + instance1.getFirstName());
+         System.out.println("Policyholder's Last Name: " + instance1.getLastName());
+         System.out.println("Policyholder's Age: " + instance1.getAge());
+         System.out.println("Policyholder's Smoking Status: " + instance1.getSmokingStatus());
+         System.out.printf("Policyholder's Height: %.1f inches", instance1.getHeight());
+         System.out.printf("\nPolicyholder's Weight: %.1f pounds", instance1.getWeight());
+         System.out.printf("\nPolicyholder's BMI: %.2f", instance1.getBMI());
+         System.out.printf("\nPolicy Price: $%.2f", instance1.getPricePolicy(age, smokingStatus));
+
+      } //end while
+     
+      inputFile.close(); //close the file  
+                      
    
    } //end main
 } //end class
+
